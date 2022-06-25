1fef3fad48bd27783daf0cff1008d71feb3d5bd5
Aditya-Y-Git <68696120+Aditya-Y-Git@users.noreply.github.com>
2022-06-26 00:13:19 +0530

    Greedy Algorithm

---
+++ Minimum Number of Platforms.cpp
@@ -0,0 +1,21 @@
+int calculateMinPatforms(int at[], int dt[], int n) {
+    // Write your code here.
+    sort(at,at+n);
+    sort(dt,dt+n);
+    int plat = 1,ans=1;
+    int i=1,j=0;
+    while(i<n && j<n){
+        if(at[i] <= dt[j]){
+            plat++;
+            i++;
+        } 
+        else if(at[i] > dt[j]){
+            plat--;
+            j++;
+        }
+        if(plat > ans){
+            ans = plat;
+        }
+    }
+    return ans;
+}
